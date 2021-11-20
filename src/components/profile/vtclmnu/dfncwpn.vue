69ede690c05dbe80738593fb31ed5b949a902ae1
Hassan-Shahriar-1 <hassanshahriar18@gmail.com>
2021-11-20 19:28:10 +0600

    updata last today

--- src/components/profile/vtclmnu/dfncwpn.vue
+++ src/components/profile/vtclmnu/dfncwpn.vue
@@ -3,24 +3,24 @@
     <h6 class="blkgry px-1 py-2" v-if="err==''"> You can't have more than 5 Item in a single Slot</h6>
      <h6 class="blkgry px-1 py-2" v-else> {{ err }}</h6>
     <div class="row Gp0m0">
-        <div class="col-3 Gp0m0 bdr1 " @click="show(0);"><img v-if="img[0]!=''" class="card-img" src="../../../assets/img/1.png" alt="" ></div>
-        <div class="col-3 Gp0m0 bdr1 " @click="show(1);"><img   class="card-img" src="../../../assets/img/1.png" alt=""></div>
-        <div class="col-3 Gp0m0 bdr1 " @click="show(2);"><img   class="card-img" src="../../../assets/img/1.png" alt=""></div>
-        <div class="col-3 Gp0m0 bdr1 " @click="show(3);"><img   class="card-img" src="../../../assets/img/1.png" alt=""></div>
-        <div class="col-3 Gp0m0 bdr1 " @click="show(4);"><img   class="card-img" src="../../../assets/img/1.png" alt=""></div>
+        <div class="col-3 Gp0m0 bdr1 " @click="show(0);"><img v-if="attck.length!=0" :src="imglnk+attck[0]+'.png'" class="card-img" alt="" ><img  v-else class="card-img" src="../../../assets/img/1.png" alt=""></div>
+        <div class="col-3 Gp0m0 bdr1 " @click="show(1);"><img v-if="attck.length!=0" :src="imglnk+attck[1]+'.png'" class="card-img" alt="" ><img  v-else class="card-img" src="../../../assets/img/1.png" alt=""></div>
+        <div class="col-3 Gp0m0 bdr1 " @click="show(2);"><img v-if="attck.length!=0" :src="imglnk+attck[2]+'.png'" class="card-img" alt="" ><img  v-else class="card-img" src="../../../assets/img/1.png" alt=""></div>
+        <div class="col-3 Gp0m0 bdr1 " @click="show(3);"><img v-if="attck.length!=0" :src="imglnk+attck[3]+'.png'" class="card-img" alt="" ><img  v-else class="card-img" src="../../../assets/img/1.png" alt=""></div>
+        <div class="col-3 Gp0m0 bdr1 " @click="show(4);"><img v-if="attck.length!=0" :src="imglnk+attck[4]+'.png'" class="card-img" alt="" ><img  v-else class="card-img" src="../../../assets/img/1.png" alt=""></div>
         
-        <div class="col-3 Gp0m0 bdr1 " @click="show(5);"><img   class="card-img" src="../../../assets/img/1.png" alt=""></div>
-        <div class="col-3 Gp0m0 bdr1 " @click="show(6);"><img   class="card-img" src="../../../assets/img/1.png" alt=""></div>
-        <div class="col-3 Gp0m0 bdr1 " @click="show(7);"><img   class="card-img" src="../../../assets/img/1.png" alt=""></div>
-        <div class="col-3 Gp0m0 bdr1 " @click="show(8);"><img   class="card-img" src="../../../assets/img/1.png" alt=""></div>
-        <div class="col-3 Gp0m0 bdr1 " @click="show(9);"><img   class="card-img" src="../../../assets/img/1.png" alt=""></div>
+        <div class="col-3 Gp0m0 bdr1 " @click="show(5);"><img v-if="attck.length!=0" :src="imglnk+attck[5]+'.png'" class="card-img" alt="" ><img  v-else class="card-img" src="../../../assets/img/1.png" alt=""></div>
+        <div class="col-3 Gp0m0 bdr1 " @click="show(6);"><img v-if="attck.length!=0" :src="imglnk+attck[6]+'.png'" class="card-img" alt="" ><img  v-else class="card-img" src="../../../assets/img/1.png" alt=""></div>
+        <div class="col-3 Gp0m0 bdr1 " @click="show(7);"><img v-if="attck.length!=0" :src="imglnk+attck[7]+'.png'" class="card-img" alt="" ><img  v-else class="card-img" src="../../../assets/img/1.png" alt=""></div>
+        <div class="col-3 Gp0m0 bdr1 " @click="show(8);"><img v-if="attck.length!=0" :src="imglnk+attck[8]+'.png'" class="card-img" alt="" ><img  v-else class="card-img" src="../../../assets/img/1.png" alt=""></div>
+        <div class="col-3 Gp0m0 bdr1 " @click="show(9);"><img v-if="attck.length!=0" :src="imglnk+attck[9]+'.png'" class="card-img" alt="" ><img  v-else class="card-img" src="../../../assets/img/1.png" alt=""></div>
         
-        <div class="col-3 Gp0m0 bdr1 " @click="show(10);"><img   class="card-img" src="../../../assets/img/1.png" alt=""></div>
-        <div class="col-3 Gp0m0 bdr1 " @click="show(11);"><img   class="card-img" src="../../../assets/img/1.png" alt=""></div>
-        <div class="col-3 Gp0m0 bdr1 " @click="show(12);"><img   class="card-img" src="../../../assets/img/1.png" alt=""></div>
-        <div class="col-3 Gp0m0 bdr1 " @click="show(13);"><img   class="card-img" src="../../../assets/img/1.png" alt=""></div>
-        <div class="col-3 Gp0m0 bdr1 " @click="show(14);"><img   class="card-img" src="../../../assets/img/1.png" alt=""></div>
-        <div class="col-3 Gp0m0 bdr1 " @click="show(15);"><img   class="card-img" src="../../../assets/img/1.png" alt=""></div>
+        <div class="col-3 Gp0m0 bdr1 " @click="show(10);"><img v-if="attck.length!=0" :src="imglnk+attck[10]+'.png'" class="card-img" alt="" ><img  v-else class="card-img" src="../../../assets/img/1.png" alt=""></div>
+        <div class="col-3 Gp0m0 bdr1 " @click="show(11);"><img v-if="attck.length!=0" :src="imglnk+attck[11]+'.png'" class="card-img" alt="" ><img  v-else class="card-img" src="../../../assets/img/1.png" alt=""></div>
+        <div class="col-3 Gp0m0 bdr1 " @click="show(12);"><img v-if="attck.length!=0" :src="imglnk+attck[12]+'.png'" class="card-img" alt="" ><img  v-else class="card-img" src="../../../assets/img/1.png" alt=""></div>
+        <div class="col-3 Gp0m0 bdr1 " @click="show(13);"><img v-if="attck.length!=0" :src="imglnk+attck[13]+'.png'" class="card-img" alt="" ><img  v-else class="card-img" src="../../../assets/img/1.png" alt=""></div>
+        <div class="col-3 Gp0m0 bdr1 " @click="show(14);"><img v-if="attck.length!=0" :src="imglnk+attck[14]+'.png'" class="card-img" alt="" ><img  v-else class="card-img" src="../../../assets/img/1.png" alt=""></div>
+        <div class="col-3 Gp0m0 bdr1 " @click="show(15);"><img v-if="attck.length!=0" :src="imglnk+attck[15]+'.png'" class="card-img" alt="" ><img  v-else class="card-img" src="../../../assets/img/1.png" alt=""></div>
         <div class="col-2"></div>
         <div class="col-8 mt-2" ><div class="btn nBtn nBbg bgD8 px-5" @click="save()">Save</div></div>
         <div class="col-2"></div>
@@ -63,9 +63,11 @@ export default {
 
         return{
             defense:[],
-            img:[],
+            attck:[],
             err:'',
+            imglnk:'http://mrdemo.com/item/',
             index:0,
+            dfltimg:'../../assets/img/1.png',
              dfnitm:[
                
                 {
@@ -109,6 +111,14 @@ export default {
           
         }
 
+    },
+    beforeCreate(){
+        this.$mgo.gt('threat/attack',(res)=>{
+            if(res.sts=='ok'){
+                this.attck=res.data
+            }
+            console.log(this.attck)
+        })
     },
     methods: {
                      beforeOspen(){
