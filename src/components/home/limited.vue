085aa13bee8124e5c640817e544c7e8d474dc1ce
Hassan-Shahriar-1 <hassanshahriar18@gmail.com>
2021-12-09 15:08:58 +0600

    timer fixed

--- src/components/home/limited.vue
+++ src/components/home/limited.vue
@@ -14,6 +14,21 @@
                     </div>
                 </div>
             </div>
+             <vue-countdown-timer
+      @start_callback="startCallBack('event started')"
+      @end_callback="endCallBack('event ended')"
+      :start-time="tmr"
+      :end-time="end"
+      :interval="1000"
+      :start-label="'Until start:'"
+      :end-label="' Time Left:'"
+      label-position="begin"
+      :end-text="'Event ended!'"
+      :day-txt="'days'"
+      :hour-txt="'hours'"
+      :minutes-txt="'minutes'"
+      :seconds-txt="'seconds'">
+    </vue-countdown-timer>
 
             <div class="row py-2 text-center Gp0m0"><h4 class="hr-sect jaldi">Agora Shop</h4></div>
             <div class="ptnbg blksdo p-2 m-2">
@@ -68,10 +83,16 @@ export default{
             fml:[],
             avtst:[],
             bg:[],
+            tmr:'',
+            end:''
+
         }
     },
     beforeCreate(){
       this.$mgo.gt('mp/limited/editiopn',(rs)=>{
+          this.tmr=rs.data['tmr'][0]['sttm'];
+          this.end=rs.data['tmr'][0]['endtm']; 
+          console.log(this.tmr);
           console.log(rs);
       })
     }
